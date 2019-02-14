e1760804b7e1cfa383330ff02ee0023ef8f35386
RABC <RABCbot@users.noreply.github.com>
2019-02-14 12:59:35 -0500

    Add files via upload

--- sensor/butler_sensor.py
+++ sensor/butler_sensor.py
@@ -1,79 +1,191 @@
+import sys
+import signal
 import logging
 import json
 import time
-import requests
+import threading
 import paho.mqtt.client as mqtt
 from hcsr04sensor import sensor
 
-CONFIG_FILE = 'config.json'
-LOGGER_FILE = 'butler-sensor.log'
+CONFIG_FILE = 'butler_sensor.json'
+LOGGER_FILE = 'butler_sensor.log'
 
-logging.basicConfig(level=logging.WARNING,
+def handle_exit(sig, frame):
+    raise(SystemExit)
+
+signal.signal(signal.SIGTERM, handle_exit)
+
+logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s',
                     filename=LOGGER_FILE,
                     filemode='w')
 _LOGGER = logging.getLogger(__name__)
 
+class Config():
+    def __init__(self, filename):
+        self._json = {}
+        try:        
+            with open(filename, 'r') as f:
+                self._json = json.load(f)
+        except IOError as ex:
+            _LOGGER.error('Failed to read configuration file, because %s', ex)
+            raise
 
-class MqttDistance():
+    @property
+    def Json(self):
+        return self._json
 
-    def __init__(self):
-        self._prev_presence = "OFF"
+    def Write(self, filename):
+        try:
+            with open(filename, 'w') as f:
+                json.dump(self._json, f)
+        except IOError as ex:
+            _LOGGER.error('Failed to write configuration file, because %s', ex)
+            raise
+
+
+class Mqtt():
+
+    def __init__(self, host, topic):
+        self._client = mqtt.Client()
+        self._topic_prefix = topic
+        self._client.on_connect = self.top_on_connect
+        self._client.on_message = self.top_on_message
+        self._client.connect(host, 1883, 60)
+        self._on_connect = None
+        self._on_message = None
+
+    def start(self, forever):
+        _LOGGER.debug('Mqtt loop starting...')
+        if forever:
+            self._client.loop_forever()
+        else:
+            self._client.loop_start()
+
+    def stop(self):
+        _LOGGER.debug('Mqtt loop stopping...')
+        self._client.loop_stop()
+
+    @property
+    def on_connect(self):
+        return self._on_connect
+
+    @on_connect.setter
+    def on_connect(self, func):
+        self._on_connect = func
+
+    def top_on_connect(self, client, userdata, flags, rc):
+        _LOGGER.debug('Mqtt connected')
+        self._client.subscribe(self._topic_prefix + 'set/#')
+        self._client.subscribe(self._topic_prefix + 'get/#')
+        self._client.subscribe(self._topic_prefix + 'run/#')
+        if self._on_connect is not None:
+            self._on_connect(client, userdata, flags, rc)
+
+    @property
+    def on_message(self):
+        return self._on_message
+
+    @on_message.setter
+    def on_message(self, func):
+        self._on_message = func
+
+    def top_on_message(self, client, userdata, msg):
+        _LOGGER.debug('Message received: %s, payload: %s', msg.topic, msg.payload.decode('ascii'))
+        if self._on_message is not None:
+            self._on_message(client, userdata, msg)
+
+    def publish(self, topic, payload):
+        self._client.publish(topic, payload)
+
+class Worker():
+
+    def __init__(self, config):
+        self._prev_presence = None
         self._prev_time = time.time()
         # config parameters
-        self._mqtt_host = 'iot.eclipse.org'
-        self._trigger_pin = 17
-        self._echo_pin = 27
-        self._trigger_temp = 68
-        self._trigger_distance = 20
-        self._unit_system = 'imperial'
-        self._time_interval = 60
-        self._publish_distance = False
-        self._topic_prefix = 'home/butler/sensor/'
-        self.read_config()
+        self._trigger_pin = config['trigger_pin']
+        self._echo_pin = config['echo_pin']
+        self._sensor_temperature = config['sensor_temperature']
+        self._distance_threshold =  config['distance_threshold']
+        self._unit_system =  config['unit_system']
+        self._time_interval = config['time_interval']
+        self._publish_distance = config['publish_distance']
+        # Mqtt
+        self._topic_prefix = config['topic_prefix']
+        self._mqtt = Mqtt(config['mqtt_host'], config['topic_prefix'])
+        self._mqtt.on_message = self.on_message
         # Distance sensor
         self._sensor = sensor.Measurement(self._trigger_pin,
                                           self._echo_pin,
-                                          temperature = self._trigger_temp,
+                                          temperature = self._sensor_temperature,
                                           unit = self._unit_system,
                                           round_to=2)
-        # Mqtt
-        self._client = mqtt.Client()
-        self._client.on_connect = self.on_connect
-        self._client.on_message = self.on_message
-        self._client.connect(self._mqtt_host, 1883, 60)
-        # self._client.loop_start()
+        _LOGGER.debug('Worker initialized')
 
-        _LOGGER.debug('MqttDistance initialized')
+    def on_message(self, client, userdata, msg):
+        t = msg.topic.rsplit('/', 2)
+        verb = t[1]
+        key = t[2]
+        value = msg.payload.decode('ascii')
+        if key == 'mqtt_host':
+            self._mqtt_host = value
+        if key == 'trigger_pin':
+            self._trigger_pin = int(value)
+        if key == 'echo_pin':
+            self._echo_pin = int(value)
+        if key == 'sensor_temperature':
+            self._sensor_temperature = int(value)
+        if key == 'unit_system':
+            self._unit_system =  value
+        if key == 'time_interval':
+            self._time_interval = int(value)
+        if key == 'distance_threshold':
+            self._distance_threshold = round(float(value))
+        if key == 'publish_distance':
+            self._publish_distance = (value == 'true')
+        if key == 'topic_prefix':
+            self._topic_prefix = value
+
+
+        #if key == 'config':
+        #    config = json.dumps(self.get_config())
+        #    _LOGGER.debug(config)
+        #    self._mqtt.publish(self._topic_prefix + 'config', config)
+        #if key == 'write':
+        #    self.write_config()
 
     def loop(self):
-        self._client.loop_start()
         distance = 0
         presence = 'OFF'
+        self._mqtt.start(False)
         _LOGGER.debug('Loop started')
+       
         while True:
             try:
                 distance = self.read_distance()
                 _LOGGER.debug('Distance %s', distance)
                 if self._publish_distance:
-                    self._client.publish(self._topic_prefix + 'distance', distance)
-                if distance < self._trigger_distance:
+                    self._mqtt.publish(self._topic_prefix + 'distance', distance)
+                if distance < self._distance_threshold:
                     presence = 'ON'
                 else:
                     presence = 'OFF'
-            except:
+                if presence != self._prev_presence or time.time() - self._prev_time > self._time_interval:
+                    _LOGGER.debug('Publish presence %s', presence)
+                    self._mqtt.publish(self._topic_prefix + 'presence', presence)
+                    self._prev_presence = presence
+                    self._prev_time = time.time()
+            except (KeyboardInterrupt, SystemExit):
+                self._mqtt.stop()
+                _LOGGER.debug('Loop stopped')
+                sys.exit()
+            except (UnboundLocalError):
                 pass
-
-            if presence != self._prev_presence or time.time() - self._prev_time > self._time_interval:
-                _LOGGER.debug('Publish presence %s', presence)
-                self._client.publish(self._topic_prefix + 'presence', presence)
-                self._prev_presence = presence
-                self._prev_time = time.time() 
-
-    def stop(self):
-        self._client.loop_stop()
-        _LOGGER.debug('Loop stopped')
-        sys.exit()
+            except Exception as ex:
+                _LOGGER.error(str(ex))
+                raise
+                
 
     def read_distance(self):
         raw = self._sensor.raw_distance()
@@ -82,80 +194,10 @@ class MqttDistance():
         if self._unit_system == 'metric':
             return self._sensor.distance_metric(raw)
 
-    def read_config(self):
-        try:        
-            with open(CONFIG_FILE, 'r') as f:
-                config = json.load(f)
-                _LOGGER.debug(config)
-                self._mqtt_host = config['mqtt-host']
-                self._trigger_pin = config['trigger-pin']
-                self._echo_pin = config['echo-pin']
-                self._trigger_temp = config['trigger-temperature']
-                self._trigger_distance =  config['trigger-distance']
-                self._unit_system =  config['unit-system']
-                self._time_interval = config['time-interval']
-                self._publish_distance = config['publish-distance']
-                self._topic_prefix = config['topic-prefix']
-        except IOError as ex:
-            _LOGGER.error('Failed to read configuration file, because %s', ex)
-            sys.exit()
-
-    def get_config(self):
-        return {'mqtt-host': self._mqtt_host,
-                'trigger-pin': self._trigger_pin,
-                'echo-pin': self._echo_pin,
-                'trigger-temperature': self._trigger_temp,
-                'trigger-distance': self._trigger_distance,
-                'unit-system': self._unit_system,
-                'time-interval': self._time_interval,
-                'publish-distance': self._publish_distance,
-                'topic-prefix': self._topic_prefix}
-
-    def write_config(self):
-        try:
-            config = self.get_config()
-            with open('config.json', 'w') as f:
-                json.dump(config, f)
-        except IOError as ex:
-            _LOGGER.error('Failed to write configuration file, because %s', ex)
-
-    def on_connect(self, client, userdata, flags, rc):
-        _LOGGER.debug('Mqtt connected')
-        self._client.subscribe(self._topic_prefix + 'config/#')
-
-    def on_message(self, client, userdata, msg):
-        key = msg.topic.rsplit('/', 1)[1]
-        value = msg.payload.decode('ascii')
-        _LOGGER.debug('Message received: %s, payload: %s', key, value)
-        if key == 'mqtt-host':
-            self._mqtt_host = value
-        if key == 'trigger-pin':
-            self._trigger_pin = int(value)
-        if key == 'echo-pin':
-            self._echo_pin = int(value)
-        if key == 'trigger-temperature':
-            self._trigger_temp = int(value)
-        if key == 'trigger-distance':
-            self._trigger_distance =  int(value)
-        if key == 'unit-system':
-            self._unit_system =  value
-        if key == 'time-interval':
-            self._time_interval = int(value)
-        if key == 'publish-distance':
-            self._publish_distance = (value == 'true')
-        if key == 'topic-prefix':
-            self._topic_prefix = value
-        if key == 'get':
-            config = json.dumps(self.get_config())
-            _LOGGER.debug(config)
-            self._client.publish(self._topic_prefix + 'config', config)
-        if key == 'write':
-            self.write_config()
-
 if __name__ == "__main__":
+    # print(sys.argv[0])
+    c = Config(CONFIG_FILE)
+    w = Worker(c.Json)
+    w.loop()
 
-    bot = MqttDistance()
-    try:
-        bot.loop()
-    except:
-        bot.stop()
+

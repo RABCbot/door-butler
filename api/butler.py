e25b70349d9d4bf2914a4789c52f51c88756d151
Ricardo Balladares <RABCbot@users.noreply.github.com>
2020-05-21 08:17:44 -0400

    Add files via upload

--- api/butler.py
+++ api/butler.py
@@ -6,7 +6,7 @@ from flask import Flask, request
 
 
 app = Flask(__name__)
-logging.basicConfig(filename="butler.log",level=logging.ERROR)
+logging.basicConfig(filename="butler.log",level=logging.DEBUG)
 
 ok = {"status": "ok"}
 
@@ -15,15 +15,18 @@ def status():
   return json.dumps(ok)
 
 
-@app.route("/api/command", methods=["POST"])
+@app.route("/api/run", methods=["POST"])
 def command():
   msg = ok
   status = 200
   try:
     data = request.json
-    subprocess.run(data["arguments"].split(" "))
+    for cmd in data["commands"]:
+      args = cmd["args"]
+      subprocess.check_call(args.split(" "), timeout=60)
+      logging.debug(args)
   except Exception as ex:
-    msg = {"status": "exception", "message":re.sub(r"[\000-\011\013-\037]", "", str(ex))}
+    msg = {"status": "exception", "message":str(ex)}
     status = 500
     logging.error(ex)
   return json.dumps(msg), status

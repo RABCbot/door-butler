9ea37afed24832219624a71696036a15f8e269e6
Ricardo Balladares <RABCbot@users.noreply.github.com>
2020-05-26 10:34:22 -0400

    shell=True

--- api/butler.py
+++ api/butler.py
@@ -1,9 +1,7 @@
-import re
 import subprocess
 import logging
 import json
 from flask import Flask, request
-
 
 app = Flask(__name__)
 logging.basicConfig(filename="butler.log",level=logging.DEBUG)
@@ -22,9 +20,8 @@ def command():
   try:
     data = request.json
     for cmd in data["commands"]:
-      args = cmd["args"]
-      subprocess.check_call(args.split(" "), timeout=60)
-      logging.debug(args)
+      logging.debug(cmd)
+      subprocess.check_call(cmd["args"], shell=True, timeout=60)
   except Exception as ex:
     msg = {"status": "exception", "message":str(ex)}
     status = 500
@@ -34,3 +31,4 @@ def command():
 if __name__ == "__main__":
   app.debug = True
   app.run(host="0.0.0.0", port=8080)
+
